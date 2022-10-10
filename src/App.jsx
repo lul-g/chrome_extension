12d62f425756cd3f70daf1e2cd12274d18f0270d
Lulseged Admasu <lulsegedwork@gmail.comu>
2022-10-10 02:13:10 -0400

    pre-deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Item from './Item'
+import uniqid from 'uniqid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+  const [style, setStyle] = useState({})
 
+  function save() {
+    let input = document.getElementById('in')
+    if(!input.value) {
+      setStyle(() => (
+        {
+          boxShadow: "0 0 9px 5px #e2125e",
+        }))
+      return 
+    }
+    else {
+      let val =input.value
+      setList(prev => [...prev, val])
+      setStyle(() => {
+        return {
+          boxShadow: "0 0 9px 5px #008087",
+        }
+      })
+      input.value = ''
+    }
+    
+  }
+  let count = 0
   return (
 
     <div className="App">
-      <input type="text" placeholder='your text here' />
-      <button>Save</button>
+      <input style={style} id="in" type="text" placeholder='your text here' />
+      <button onClick={save}>Save</button>
+      {list.map(el => <Item key={uniqid} item={el}/>)}
     </div>
   )
 }
---
+++ src/Item.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'
+
+export default function Item(props) {
+    return (
+        <div className="item">{props.item}</div>
+    )
+}
