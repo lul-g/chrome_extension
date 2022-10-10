61981e85fa07445e1cbec27d6b515f6f7abb85f4
Lulseged Admasu <lulsegedwork@gmail.comu>
2022-10-10 15:12:17 -0400

    updated to add url

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Item from './Item'
 import uniqid from 'uniqid';
 
 function App() {
-  const [list, setList] = useState([])
+  const [list, setList] = useState(
+      JSON.parse(localStorage.getItem('arr')) || []
+    )
   const [style, setStyle] = useState({})
-
+  const[url, setUrl] = useState('')
+  
+  let urlSave = false
+  function tab() {
+    setList(prev => [...prev, window.location.href])
+  }
+  
+  function clear() {
+    localStorage.clear()
+    setList(() =>  [])
+    setStyle(() => {
+      return {
+        boxShadow: "0 0 9px 5px #008087",
+      }
+    })
+    
+  }
   function save() {
     let input = document.getElementById('in')
     if(!input.value) {
@@ -18,7 +36,8 @@ function App() {
     }
     else {
       let val =input.value
-      setList(prev => [...prev, val])
+      setList(prev =>  [...prev, val])
+      
       setStyle(() => {
         return {
           boxShadow: "0 0 9px 5px #008087",
@@ -26,17 +45,26 @@ function App() {
       })
       input.value = ''
     }
-    
+
   }
   let count = 0
+  useEffect(() => {
+    localStorage.setItem('arr', JSON.stringify(list))
+  }, [list])
+
   return (
 
     <div className="App">
       <input style={style} id="in" type="text" placeholder='your text here' />
-      <button onClick={save}>Save</button>
-      {list.map(el => <Item key={uniqid} item={el}/>)}
+      <div className="btns">
+        <button onClick={save}>Save</button>
+        <button onClick={tab}>Add tab</button>
+        <button onClick={clear}>Clear</button>
+      </div>
+      {list.map(el => <Item key={++count} item={el}/>)}
     </div>
   )
+  
 }
 
 export default App
